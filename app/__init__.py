1c3d23f86b0c83b891fc7cfd4c314d425646c6ac
Marialaura-bs <133356931+Marialaura-bs@users.noreply.github.com>
2025-05-27 22:27:01 +0000

    salvando

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from .config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-
-    # Registro de rotas
+    app.config.from_object(Config)
+    app.json.sort_keys = False
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
     from .routes.messages import messages_bp
     app.register_blueprint(messages_bp, url_prefix="/messages")
 

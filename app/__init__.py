feab2b14cfa039d1af2069a5f12e6e69cb7c1c77
Marialaura-bs <133356931+Marialaura-bs@users.noreply.github.com>
2025-05-27 23:08:38 +0000

    salvando

--- app/__init__.py
+++ app/__init__.py
@@ -8,11 +8,14 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(Config)  # aplica as configurações
+    # Desativa ordenação alfabética das chaves JSON
     app.json.sort_keys = False
-    db.init_app(app)
+
+    db.init_app(app)  # associa o SQLAlchemy à aplicação Flask
+
     migrate.init_app(app, db)
-    
+
     from .routes.messages import messages_bp
     app.register_blueprint(messages_bp, url_prefix="/messages")
 

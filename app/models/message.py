a6ef66e9d9ba0821d57c8d400a13fbcafe04bbb3
Theo Davi <150192145+theo-davi@users.noreply.github.com>
2025-06-30 10:35:27 +0000

    removendo código desnecessário

--- app/models/message.py
+++ app/models/message.py
@@ -1,13 +1,12 @@
 from datetime import datetime
 from app import db
-from app.models.usuario import Usuario
 
 class Message(db.Model):
     __tablename__ = 'messages'
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    autor = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_mensagens_user_id'))
+    autor = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def to_dict(self):
         return {

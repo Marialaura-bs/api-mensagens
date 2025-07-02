eee4af0025e8a44331854897e6a97b54ce0cd8d7
Marialaura-bs <133356931+Marialaura-bs@users.noreply.github.com>
2025-07-02 14:43:02 +0000

    salvando alterações

--- app/routes/messages.py
+++ app/routes/messages.py
@@ -56,7 +56,43 @@ def delete_message(message_id):
 comentario_schema = ComentarioSchema()
 comentarios_schema = ComentarioSchema(many=True)
 
+
 @messages_bp.route('/<int:message_id>/comentarios', methods=['GET'])
-def get_comentario(message_id):
-    comentario = Comentario.query.get_or_404(message_id)
+def get_comentarios_by_message(message_id):
+    comentarios = Comentario.query.filter_by(mensagem=message_id).all()
+    return comentarios_schema.jsonify(comentarios), 200
+
+@messages_bp.route('/<int:message_id>/comentarios', methods=['POST'])
+def create_comentario(message_id):
+    data = request.get_json()
+
+    # Cria nova mensagem vinculando ao usuário de ID 1
+    if message_id:
+        novo_comentario = Comentario(
+            content=data.get('content'),
+            autor=1,  # <- usuário padrão
+            mensagem=message_id
+        )
+
+    db.session.add(novo_comentario)
+    db.session.commit()
+
+    return comentario_schema.jsonify(novo_comentario), 201
+
+@messages_bp.route('/<int:message_id>/comentarios/<int:comentario_id>', methods=['PUT'])
+def update_comentario(comentario_id):
+    comentario = Comentario.query.get_or_404(comentario_id)
+    data = message_schema.load(request.get_json(), partial=True)
+
+    if 'content' in request.get_json():
+        comentario.content = data.content
+
+    db.session.commit()
     return comentario_schema.jsonify(comentario), 200
+
+@messages_bp.route('/<int:message_id>', methods=['DELETE'])
+def delete_message(message_id):
+    message = Message.query.get_or_404(message_id)
+    db.session.delete(message)
+    db.session.commit()
+    return '', 204

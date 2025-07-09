50a528b5726c728b01cd612e3296c58504012853
Theo Davi <150192145+theo-davi@users.noreply.github.com>
2025-07-09 14:01:48 +0000

    atualizando rotas de get_comentarios e create_comentarios

--- app/routes/messages.py
+++ app/routes/messages.py
@@ -21,8 +21,7 @@ def get_message(message_id):
 
 @messages_bp.route('/', methods=['POST'])
 def create_message():
-    data = request.get_json()
-
+    data = MessageSchema.load(request.get_json())
     # Cria nova mensagem vinculando ao usuário de ID 1
     nova_mensagem = Message(
         content=data.get('content'),
@@ -37,7 +36,7 @@ def create_message():
 @messages_bp.route('/<int:message_id>', methods=['PUT'])
 def update_message(message_id):
     message = Message.query.get_or_404(message_id)
-    data = message_schema.load(request.get_json(), partial=True)
+    data = message_schema.load(request.get_json(), partial=True)#esse partial=True é necessário?
 
     if 'content' in request.get_json():
         message.content = data.content
@@ -52,37 +51,33 @@ def delete_message(message_id):
     db.session.commit()
     return '', 204
 
-
 comentario_schema = ComentarioSchema()
 comentarios_schema = ComentarioSchema(many=True)
 
-
 @messages_bp.route('/<int:message_id>/comentarios', methods=['GET'])
-def get_comentarios_by_message(message_id):
+def get_comentarios(message_id):
     comentarios = Comentario.query.filter_by(mensagem=message_id).all()
     return comentarios_schema.jsonify(comentarios), 200
 
 @messages_bp.route('/<int:message_id>/comentarios', methods=['POST'])
 def create_comentario(message_id):
     data = request.get_json()
-
+    message=Message.query.get_or_404(message_id)
     # Cria nova mensagem vinculando ao usuário de ID 1
-    if message_id:
-        novo_comentario = Comentario(
-            content=data.get('content'),
-            autor=1,  # <- usuário padrão
-            mensagem=message_id
-        )
-
+    novo_comentario = Comentario(
+        content=data.get('content'),
+        autor=1,  # <- usuário padrão
+        mensagem=message_id
+    )
     db.session.add(novo_comentario)
     db.session.commit()
-
     return comentario_schema.jsonify(novo_comentario), 201
 
 @messages_bp.route('/<int:message_id>/comentarios/<int:comentario_id>', methods=['PUT'])
-def update_comentario(comentario_id):
+def update_comentario(message_id, comentario_id):
+    message=Message.query.get_or_404(message_id)
     comentario = Comentario.query.get_or_404(comentario_id)
-    data = message_schema.load(request.get_json(), partial=True)
+    data = comentario_schema.load(request.get_json(), partial=True)#por que o message_id não é atualizado?
 
     if 'content' in request.get_json():
         comentario.content = data.content
@@ -90,9 +85,10 @@ def update_comentario(comentario_id):
     db.session.commit()
     return comentario_schema.jsonify(comentario), 200
 
-@messages_bp.route('/<int:message_id>', methods=['DELETE'])
-def delete_message(message_id):
+@messages_bp.route('/<int:message_id>comentarios/<int:comentario_id>', methods=['DELETE'])
+def delete_comentario(message_id, comentario_id):
     message = Message.query.get_or_404(message_id)
+    comentario = Comentario.query.get_or_404(comentario_id)
     db.session.delete(message)
     db.session.commit()
     return '', 204

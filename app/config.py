9f15eba905255971fb24feb579e43fcd07ccd397
Marialaura-bs <133356931+Marialaura-bs@users.noreply.github.com>
2025-08-07 19:00:47 +0000

    Co-authored-by: Theo Davi Nunes de Lima <theo.davi@academico.ifrn.edu.br>

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -6,3 +7,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'messages.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY='theoelaurabests'
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15) # Access tokens expire after 15 minutes
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30) # Refresh tokens expire after 30 days
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_refresh_token_required
 from werkzeug.security import check_password_hash
 from ..models.usuario import Usuario
 from .. import db
@@ -17,4 +17,12 @@ def login():
         return jsonify({"error": "Credenciais inválidas"}), 401
 
     token = create_access_token(identity=user.id)
-    return jsonify({"access_token": token}), 200
+    refresh_token = create_refresh_token(identity=user.id)
+    return jsonify({"access_token": token, "refresh_token":refresh_token}), 200
+
+@auth_bp.route("/refresh", methods=["POST"])
+@jwt_refresh_token_required
+def refresh():
+    current_user = get_jwt_identity()
+    new_access_token = create_access_token(identity=current_user)
+    return jsonify({"access_token"=new_access_token}), 200

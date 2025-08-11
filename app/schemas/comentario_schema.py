cca3420b1284dc2701253544dfaca76651649a2f
Theo Davi <150192145+theo-davi@users.noreply.github.com>
2025-08-11 11:50:33 +0000

    att

--- app/schemas/comentario_schema.py
+++ app/schemas/comentario_schema.py
@@ -8,7 +8,7 @@ class ComentarioSchema(ma.SQLAlchemyAutoSchema):
         load_instance = False #permite o desempacotamento do dicionário no comment_controller.criar_comentario
         fields = ("id", "usuario_id", "mensagem_id", "conteudo", "data_criacao")
     id = fields.Int(dump_only=True)
-    usuario_id = fields.Int(dump_only=True)
-    mensagem_id = fields.Int(dump_only=True)  # <- alterado aqui
+    usuario_id = fields.Int(load_only=True)
+    mensagem_id = fields.Int(load_only=True)  # <- alterado aqui
     conteudo = fields.Str(required=True, validate=validate.Length(min=1))
     data_criacao = fields.DateTime(dump_only=True)
